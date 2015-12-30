65edb3b7d8c21d1379ad998d8bd542766ade25a5
Southclaw <southclawjk@gmail.com>
2015-12-30 19:27:59 +0000

    Added logger project to repo

---
+++ logger/Logger.h
@@ -0,0 +1,18 @@
+/*
+	Logger
+
+	Barnaby Keene 2015
+*/
+
+#ifndef LOGGER_INCLUDED
+#define LOGGER_INCLUDED
+
+class Logger
+{
+public:
+Logger();
+
+private:
+};
+
+#endif
---
+++ logger/main.cpp
@@ -0,0 +1,24 @@
+/*
+	Test code for logger module.
+*/
+
+
+#include "stdafx.h"
+#include <iostream>
+
+#include "Logger.h"
+
+
+using namespace std;
+
+
+int main()
+{
+	Logger log;
+
+	cout << "Press Enter to close." << endl;
+	getchar();
+
+    return 0;
+}
+
