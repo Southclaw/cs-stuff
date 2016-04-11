0bd22d20af97ccf6750cdeb2500f93342461ef52
Barnaby Keene 2014 (N0581491) <N0581491@my.ntu.ac.uk>
2016-04-11 11:25:59 +0100

    Added user class

---
+++ SocialMusicServer/src/socialmusicserver/User.java
@@ -0,0 +1,16 @@
+package socialmusicserver;
+
+
+public class User
+{
+	String Username;
+	String Password;
+	String Location;
+	String DateOfBirth;
+	String Information;
+
+	String MusicProfile[];
+	String Friends[];
+	String SharedSongs[];
+	String FriendRequests[];
+}
